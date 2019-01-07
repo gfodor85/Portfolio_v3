dde3cf4419891fc8609c1ed5651b6af3259bd1a7
György Fodor <gfodor85@gmail.com>
2019-01-07 19:37:20 +0100

    -

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
 
   // $('.nav-item-wrapper').animate({left: '0px'}, 1000);
-  $('.panel-about').fadeIn(300, function(){
+  // Vissza kell írni panel-about-ra!
+  $('.panel-work').fadeIn(300, function(){
     $(this).addClass('active');
   });
 
